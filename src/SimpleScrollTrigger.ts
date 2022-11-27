fc9acd952978b419f82337772d704730edd85fc5
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2022-11-28 00:13:57 +0900

    スタートトリガー作成

--- src/SimpleScrollTrigger.ts
+++ src/SimpleScrollTrigger.ts
@@ -1,19 +1,65 @@
+type Options = {
+  trigger: Element;
+  onEnter: () => void;
+  startViewPortPoint?: number;
+  startTriggerPoint?: number;
+};
 export class SimpleScrollTrigger {
-  constructor(trigger: Element, onEnter: () => void) {
-    this.triggerElemet = trigger;
-    this.observer.observe(this.triggerElemet);
-    this.onEnterCallback = onEnter;
-  }
   private triggerElemet: Element;
 
   private onEnterCallback: () => void;
 
-  private callback(entries: IntersectionObserverEntry[]) {
-    console.log(entries);
+  private observer: IntersectionObserver | null = null;
+
+  private startViewPortPoint: number;
+  private startTriggerPoint: number;
+  constructor({
+    trigger,
+    onEnter,
+    startViewPortPoint,
+    startTriggerPoint,
+  }: Options) {
+    this.triggerElemet = trigger;
+    this.onEnterCallback = onEnter;
+
+    this.startViewPortPoint =
+      startViewPortPoint === undefined ? 0 : startViewPortPoint;
+    this.startTriggerPoint =
+      startTriggerPoint === undefined ? 0 : startTriggerPoint;
+    console.log(startTriggerPoint, this.startTriggerPoint);
+    this.setupObserver();
   }
 
-  observer = new IntersectionObserver(this.callback, {
-    rootMargin: "",
-    threshold: [0,0.01,0.02,0.03,0.04,0.05,0.1,0.2,],
-  });
+  setupObserver() {
+    // 閾値の配列作成
+    const threshold = [0];
+    for (let i = 1; i < 100; i++) {
+      threshold.push(i / 100);
+    }
+
+    const windowHeight = window.innerHeight;
+    const targetElementHeight = this.triggerElemet.clientHeight;
+
+    const heightDiff = targetElementHeight - windowHeight;
+    const bottomMargin =
+      -(windowHeight * (100 - this.startViewPortPoint)) / 100;
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      console.log(entries[0].intersectionRatio);
+      if (
+        entries[0].isIntersecting &&
+        entries[0].intersectionRatio >= this.startTriggerPoint / 100
+      ) {
+        this.onEnterCallback();
+      }
+    };
+
+    console.log(`${heightDiff - bottomMargin}px 0px ${bottomMargin}px`);
+    this.observer = new IntersectionObserver(callback, {
+      rootMargin: `${heightDiff - bottomMargin}px 0px ${bottomMargin}px`,
+      threshold: threshold,
+    });
+
+    this.observer.observe(this.triggerElemet);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { SimpleScrollTrigger } from "./SimpleScrollTrigger";
 
 const trigger = document.querySelector(".trigger");
-if(trigger){
-  new SimpleScrollTrigger(trigger,()=>{console.log("onEnter")})
+if (trigger) {
+  new SimpleScrollTrigger({
+    trigger,
+    onEnter: () => {
+      console.log("onEnter");
+    },
+    startViewPortPoint: 100,
+    startTriggerPoint: 99,
+  });
 }
