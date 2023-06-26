7d59a74633728623d3804459d26403be1733190e
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-06-26 09:24:00 +0900

    初期値のテスト作成（WIP)

--- tests/e2eTest.spec.ts
+++ tests/e2eTest.spec.ts
@@ -7,24 +7,28 @@ test("Callback Test (not once)", async ({ page }) => {
   await expect(locator).toContainText("---");
 
   const elementMiddleHandle = await page.$("#middle");
-  if (elementMiddleHandle) {
-    await elementMiddleHandle.scrollIntoViewIfNeeded();
-    await expect(locator).toContainText("onEnter");
-  }
   const elementEndHandle = await page.$("#end");
-  if (elementEndHandle) {
-    await elementEndHandle.scrollIntoViewIfNeeded();
-    await expect(locator).toContainText("onLeave");
-  }
-  if (elementMiddleHandle) {
-    await elementMiddleHandle.scrollIntoViewIfNeeded();
-    await expect(locator).toContainText("onEnterBack");
-  }
   const elementStartHandle = await page.$(".scroll");
-  if (elementStartHandle) {
-    await elementStartHandle.scrollIntoViewIfNeeded();
-    await expect(locator).toContainText("onLeaveBack");
+
+  if (!elementMiddleHandle || !elementEndHandle || !elementStartHandle) {
+    throw new Error("Element is Null");
   }
+
+  // 真ん中までいったらonEnter
+  await elementMiddleHandle.scrollIntoViewIfNeeded();
+  await expect(locator).toContainText("onEnter");
+
+  // 通り過ぎたらonLeave
+  await elementEndHandle.scrollIntoViewIfNeeded();
+  await expect(locator).toContainText("onLeave");
+
+  // 戻ってきたらonEnterBack
+  await elementMiddleHandle.scrollIntoViewIfNeeded();
+  await expect(locator).toContainText("onEnterBack");
+
+  // 戻り去ったらonLeaveBack
+  await elementStartHandle.scrollIntoViewIfNeeded();
+  await expect(locator).toContainText("onLeaveBack");
 });
 
 test("Callback Test (once)", async ({ page }) => {
@@ -32,22 +36,19 @@ test("Callback Test (once)", async ({ page }) => {
 
   const locator = page.locator(".callbackText");
 
-  // ページ下までいって戻ってくる
   const elementMiddleHandle = await page.$("#middle");
-  if (elementMiddleHandle) {
-    await elementMiddleHandle.scrollIntoViewIfNeeded();
-  }
   const elementEndHandle = await page.$("#end");
-  if (elementEndHandle) {
-    await elementEndHandle.scrollIntoViewIfNeeded();
-  }
-  if (elementMiddleHandle) {
-    await elementMiddleHandle.scrollIntoViewIfNeeded();
-  }
   const elementStartHandle = await page.$(".scroll");
-  if (elementStartHandle) {
-    await elementStartHandle.scrollIntoViewIfNeeded();
+  
+  if (!elementMiddleHandle || !elementEndHandle || !elementStartHandle) {
+    throw new Error("Element is Null");
   }
+
+  // ページ下までいって戻ってくる
+  await elementMiddleHandle.scrollIntoViewIfNeeded();
+  await elementEndHandle.scrollIntoViewIfNeeded();
+  await elementMiddleHandle.scrollIntoViewIfNeeded();
+  await elementStartHandle.scrollIntoViewIfNeeded();
 
   // 戻ってきたら真ん中まで移動
   // 変わらず"onLeaveBack"のままが期待値
@@ -70,4 +71,22 @@ test("Callback Test (once)", async ({ page }) => {
     await elementStartHandle.scrollIntoViewIfNeeded();
     await expect(locator).toContainText("onLeaveBack");
   }
+});
+
+test("Initial Callback Test (start)", async ({ page }) => {
+  await page.goto("http://localhost:3333/simpleScrollTrigger/tests/03/#middle");
+
+  const locator = page.locator(".callbackText");
+
+  const elementMiddleHandle = await page.$("#middle");
+  const elementEndHandle = await page.$("#end");
+  const elementStartHandle = await page.$(".scroll");
+  
+  if (!elementMiddleHandle || !elementEndHandle || !elementStartHandle) {
+    throw new Error("Element is Null");
+  }
+
+  test.setTimeout(1000);    
+  await expect(locator).toContainText("onEnter");
+
 });
